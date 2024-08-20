6bc25851128a7c8a649f8d766d4fdbc791832249
Ruel <c0407h@gmail.com>
2024-08-20 20:49:11 +0900

    test: testcode image setup 변경
    - 프로퍼티에 직접 접근이 아닌 메서드를 통해서 접근 처리

--- Letports/LetportsTests/GatheringUploadVMTests.swift
+++ Letports/LetportsTests/GatheringUploadVMTests.swift
@@ -10,7 +10,7 @@ import Combine
 @testable import Letports
 
 final class GatheringUploadVMTests: XCTestCase {
-
+    
     var viewModel: GatheringUploadVM!
     var cancellables: Set<AnyCancellable>!
     
@@ -28,35 +28,35 @@ final class GatheringUploadVMTests: XCTestCase {
     }
     
     func testAddButtonEnableWhenAllFieldsSet() {
-           // Given - 초기화
-           let expectation = XCTestExpectation(description: "모든 입력 필드가 설정되어 true")
-           
-           // When - 설정
-           viewModel.selectedImage = UIImage()
-           viewModel.writeGatherInfo(content: "Info")
-           viewModel.writeGatherQuestion(content: "Question")
-           viewModel.writeGatehrName(content: "Test Name")
-           
-           // Then - 예상결과
-           viewModel.$addButtonEnable
-               .sink { isEnabled in
-                   if isEnabled {
-                       expectation.fulfill()
-                   }
-               }
-               .store(in: &cancellables)
-           
-           wait(for: [expectation], timeout: 1.0)
-           XCTAssertTrue(viewModel.addButtonEnable)
-       }
-
+        // Given - 초기화
+        let expectation = XCTestExpectation(description: "모든 입력 필드가 설정되어 true")
+        
+        // When - 설정
+        viewModel.changeSelectedImage(selectedImage: UIImage())
+        viewModel.writeGatherInfo(content: "Info")
+        viewModel.writeGatherQuestion(content: "Question")
+        viewModel.writeGatehrName(content: "Test Name")
+        
+        // Then - 예상결과
+        viewModel.$addButtonEnable
+            .sink { isEnabled in
+                if isEnabled {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertTrue(viewModel.addButtonEnable)
+    }
+    
     
     func testAddButtonDisableWhenFieldsAreNotSet() {
         // Given - 초기화
         let expectation = XCTestExpectation(description: "일부 입력 필드가 설정되지 않아 false")
         
         // When - 설정
-        viewModel.selectedImage = UIImage()
+        viewModel.changeSelectedImage(selectedImage: UIImage())
         viewModel.writeGatherInfo(content: "Test Info")
         viewModel.writeGatherQuestion(content: "")
         viewModel.writeGatehrName(content: "Test Name")
