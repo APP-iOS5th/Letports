5c73d628870838be22bdf2700eeb7bdcb12b358b
seonggeun an <eventscripts@nate.com>
2024-08-22 21:35:50 +0900

    feat: ProfileEditView  뷰구성 완료

---
+++ Letports/Letports/ProfileEdit/ProfileEditCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileEditCoordinator.swift
+//  Letports
+//
+//  Created by mosi on 8/19/24.
+//
+import UIKit
+
+class ProfileEditCoordinator {
+  
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start(with user: User) {
+        // ProfileEditVM을 생성하고 ProfileEditVC에 전달
+        let profileEditVM = ProfileEditVM(user: user)
+        let profileEditVC = ProfileEditVC(viewModel: profileEditVM)
+        navigationController.pushViewController(profileEditVC, animated: true)
+    }
+}
+
+
