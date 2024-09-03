823475806cf576c73e8a9527b816ca3db937550d
KuKaH <164896129+KuKaH@users.noreply.github.com>
2024-09-03 11:18:52 +0900

    Feat/hjb gathering (#68)
    * fix: 없는 파일 추가 및 GatheringTVCell Configure 부분 수정
    
    * fix: 불필요 코드 제거
    
    * feat: 생성된 소모임이 없을 시 표시 문구 추가
    
    * feat: 소모임 탭 팀변경 버튼 연결
    
    * feat: 현재 멤버와 최대 멤버가 같은 소모임 추천 소모임 목록에서 제외
    
    * feat: 현재 멤버와 최대 멤버 같을 때 소모임 리스트에서도 제외하는 코드추가 및 GatherUpload DispatchQueue 코드 추가
    
    * refactor: 코드 컨벤션 벗어난 코드 수정

--- Letports/Letports/Gathering/GatheringCoordinator.swift
+++ Letports/Letports/Gathering/GatheringCoordinator.swift
@@ -41,7 +41,9 @@ extension GatheringCoordinator: GatheringCoordinatorDelegate {
     }
     
     func presentTeamChangeController() {
-        print("팀선택")
+        let coordinator = TeamSelectionCoordinator(navigationController: navigationController)
+        coordinator.start()
+        childCoordinators.append(coordinator)
     }
     
     func pushGatheringDetailController(gatheringUid: String) {
