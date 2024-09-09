0b561fcb962ceadb9468b3ae6d45aa83b47edb03
SeongGeun-An <72062051+mo-si-dev@users.noreply.github.com>
2024-09-09 14:59:40 +0900

    Issue/sg #107 수정 (#114)
    * fix: confirm 액션만 있을경우 버튼 스타일 .default로 변경
    
    * refactor: 나가기버튼 이미지 변경 및 위치조정, 가입답변 텍스트 여백 추가
    
    * refactor:  문구 변경
    
    * feat: 소모임 삭제시 로딩뷰 추가, 소모임 삭제시 가입된 유저에게 알림 발송
    
    * fix: 로딩뷰 수정

--- Letports/Letports/Extension/Extension+UIViewController.swift
+++ Letports/Letports/Extension/Extension+UIViewController.swift
@@ -9,16 +9,17 @@ import UIKit
 
 extension UIViewController {
     func showAlert(title: String, message: String, confirmTitle: String, cancelTitle: String? = nil, onConfirm: @escaping () -> Void) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let confirmAction = UIAlertAction(title: confirmTitle, style: .destructive) { _ in onConfirm() }
-        alert.addAction(confirmAction)
-        
-        if let cancelTitle = cancelTitle {
-            let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
-            alert.addAction(cancelAction)
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            let confirmActionStyle: UIAlertAction.Style = cancelTitle == nil ? .default : .destructive
+            let confirmAction = UIAlertAction(title: confirmTitle, style: confirmActionStyle) { _ in onConfirm() }
+            alert.addAction(confirmAction)
+            
+            if let cancelTitle = cancelTitle {
+                let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
+                alert.addAction(cancelAction)
+            }
+            
+            present(alert, animated: true, completion: nil)
         }
-        
-        present(alert, animated: true, completion: nil)
-    }
 }
